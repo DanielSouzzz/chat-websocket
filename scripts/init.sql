e9955ad66dd30c73619749438a805b88881a1f5e
Danielsouzzz <danielsouzanovo@gmail.com>
2025-09-24 21:02:12 -0300

    feat: adicionar script sql para dimensionar o banco de dados

---
+++ scripts/init.sql
@@ -0,0 +1,46 @@
+use myappuser;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS chats (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    chat_type VARCHAR(7) DEFAULT 'private', -- private ou group
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS chat_participants (
+    chat_id INT NOT NULL,
+    user_id INT NOT NULL,
+
+    PRIMARY KEY (chat_id, user_id),
+    FOREIGN KEY (chat_id) REFERENCES chats(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    chat_id INT NOT NULL,
+    text TEXT,
+    sender_id INT NOT NULL,
+
+    FOREIGN KEY (chat_id) REFERENCES chats(id),
+    FOREIGN KEY (sender_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS message_status (
+    message_id INT NOT NULL,
+    user_id INT NOT NULL,
+    is_read BOOLEAN DEFAULT FALSE,
+    read_at DATETIME,
+
+    PRIMARY KEY (message_id, user_id),
+    FOREIGN KEY (message_id) REFERENCES messages(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+// TODO ADICIONAR INDICES EXTRAS PARA PERFORMANCE
