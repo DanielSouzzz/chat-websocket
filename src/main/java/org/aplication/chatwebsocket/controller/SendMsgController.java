171cf5083fef731051a22283ec9ce4b1118be249
Danielsouzzz <danielsouzanovo@gmail.com>
2025-10-17 23:52:26 -0300

    refactor: ajustar endpoint para enviar mensagens privadas

--- src/main/java/org/aplication/chatwebsocket/controller/SendMsgController.java
+++ src/main/java/org/aplication/chatwebsocket/controller/SendMsgController.java
@@ -3,22 +3,30 @@ package org.aplication.chatwebsocket.controller;
 import org.aplication.chatwebsocket.model.MessageEntity;
 import org.aplication.chatwebsocket.service.MessageService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 @RestController
 public class SendMsgController {
-
+    private final SimpMessagingTemplate messagingTemplate;
     private final MessageService messageService;
 
-    public SendMsgController(MessageService messageService) {
+    public SendMsgController(SimpMessagingTemplate messagingTemplate, MessageService messageService) {
+        this.messagingTemplate = messagingTemplate;
         this.messageService = messageService;
     }
 
-    @MessageMapping("/sendMessage")
-    @SendTo("/topic/chat/{chatId}")
-    public MessageEntity sendMessagePrivate(MessageEntity request){
+    @MessageMapping("/sendPrivateMessage")
+    public void sendMessagePrivate(MessageEntity messageRequest){
+        MessageEntity message = messageService.sendMessagePrivate(messageRequest);
 
-        return messageService.sendMessagePrivate(request);
+        messagingTemplate.convertAndSendToUser(
+                message.getReceiver().getId().toString(),
+                "/queue/messages",
+                message
+        );
+
+        messagingTemplate.convertAndSendToUser(
+                message.getSender().getId().toString(),"/queue/messages", message);
     }
 }
